06f322f1351311c8868dc663de9441102bfe94c3
Kenny Chung <kcmac@kennys-mbp.gateway.ht.net>
2018-10-08 17:10:37 -1000

    added link tag to username on photo detail

--- src/components/photoPage.jsx
+++ src/components/photoPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { loadPhoto } from '../actions/photoActions';
 
@@ -16,7 +17,9 @@ class PhotoPage extends Component {
             <img src={this.props.photo.link} alt="" />
           </div>
           <div className="details-container">
-            <div>@{this.props.photo.owner && this.props.photo.owner.username}</div>
+            <Link to={`/users/${this.props.photo.owner && this.props.photo.owner.id}`}>
+              <div>@{this.props.photo.owner && this.props.photo.owner.username}</div>
+            </Link>
             <div>{this.props.photo && this.props.photo.description}</div>
           </div>
         </div>
